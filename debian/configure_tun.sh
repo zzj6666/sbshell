3d7b5c95f14fd4cb1aceef6558d9feb3ebf075d0
qichiyu <h320117@outlook.com>
2024-12-10 16:33:50 +0800

    防火墙清理逻辑更改

--- debian/configure_tun.sh
+++ debian/configure_tun.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+# 配置参数
+TPROXY_PORT=7895  # 和 sing-box 中定义的一致
+ROUTING_MARK=666  # 和 sing-box 中定义的一致
+PROXY_FWMARK=1
+PROXY_ROUTE_TABLE=100
+INTERFACE=$(ip route show default | awk '/default/ {print $5}')
+
 # 读取当前模式
 MODE=$(grep -oP '(?<=^MODE=).*' /etc/sing-box/mode.conf)
 
+# 清理 TProxy 模式的防火墙规则
+clearTProxyRules() {
+    nft list table inet sing-box >/dev/null 2>&1 && nft delete table inet sing-box
+    ip rule del fwmark $PROXY_FWMARK lookup $PROXY_ROUTE_TABLE 2>/dev/null
+    ip route del local default dev $INTERFACE table $PROXY_ROUTE_TABLE 2>/dev/null
+    echo "清理 TProxy 模式的防火墙规则"
+}
+
 if [ "$MODE" = "TUN" ]; then
     echo "应用 TUN 模式下的防火墙规则..."
-    
+
+    # 清理 TProxy 模式的防火墙规则
+    clearTProxyRules
+
     # 确保目录存在
     sudo mkdir -p /etc/sing-box/tun
 
-    # 清理旧的规则
-    nft flush ruleset >/dev/null 2>&1
-
-    # 设置 Tun 模式的具体配置
+    # 设置 TUN 模式的具体配置
     cat > /etc/sing-box/tun/nftables.conf <<EOF
 # 清除现有的 nftables 规则并应用新的配置
 flush ruleset
@@ -23,18 +38,8 @@ table inet filter {
 }
 EOF
 
-    # 创建防火墙和 IP 路由的清空脚本
-    cat > /etc/sing-box/tun/nft-flush.sh <<EOF
-#!/bin/sh
-nft flush ruleset >/dev/null 2>&1
-ip rule delete fwmark 1 table 100 2>/dev/null
-ip route delete local default dev lo table 100 2>/dev/null
-EOF
-
-    chmod a+x /etc/sing-box/tun/nft-flush.sh
-
-    # 应用防火墙清理规则
-    sudo /etc/sing-box/tun/nft-flush.sh
+    # 应用防火墙规则
+    nft -f /etc/sing-box/tun/nftables.conf
 
     # 持久化防火墙规则
     nft list ruleset > /etc/nftables.conf
