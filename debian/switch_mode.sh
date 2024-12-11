9421e08da8f817dd6ce1a098d31a2078b73aee9d
qichiyu <h320117@outlook.com>
2024-12-11 11:32:03 +0800

    修复错误提示

--- debian/switch_mode.sh
+++ debian/switch_mode.sh
@@ -2,6 +2,7 @@
 
 # 定义颜色
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m' # 无颜色
 
 # 检查 sing-box 是否已安装
@@ -24,22 +25,25 @@ function stop_singbox() {
 # 切换模式的逻辑
 echo "切换模式开始...请根据提示输入操作。"
 
-# 选择模式
-read -rp "请选择模式(1: TProxy 模式, 2: TUN 模式）: " mode_choice
+while true; do
+    # 选择模式
+    read -rp "请选择模式(1: TProxy 模式, 2: TUN 模式): " mode_choice
 
-case $mode_choice in
-    1)
-        stop_singbox
-        echo "MODE=TProxy" | sudo tee /etc/sing-box/mode.conf > /dev/null
-        echo -e "${GREEN}当前选择模式为:TProxy 模式${NC}"
-        ;;
-    2)
-        stop_singbox
-        echo "MODE=TUN" | sudo tee /etc/sing-box/mode.conf > /dev/null
-        echo -e "${GREEN}当前选择模式为:TUN 模式${NC}"
-        ;;
-    *)
-        echo -e "${RED}无效的选择${NC}"
-        exit 1
-        ;;
-esac
+    case $mode_choice in
+        1)
+            stop_singbox
+            echo "MODE=TProxy" | sudo tee /etc/sing-box/mode.conf > /dev/null
+            echo -e "${GREEN}当前选择模式为:TProxy 模式${NC}"
+            break
+            ;;
+        2)
+            stop_singbox
+            echo "MODE=TUN" | sudo tee /etc/sing-box/mode.conf > /dev/null
+            echo -e "${GREEN}当前选择模式为:TUN 模式${NC}"
+            break
+            ;;
+        *)
+            echo -e "${RED}无效的选择，请重新输入。${NC}"
+            ;;
+    esac
+done
