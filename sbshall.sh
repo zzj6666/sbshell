78b1bf8459759c703f712d325ae3c167e12297f7
qichiyu <h320117@outlook.com>
2024-12-17 20:22:46 +0800

    删除镜像

--- sbshall.sh
+++ sbshall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义主脚本的下载URL
-MAIN_SCRIPT_URL="https://ghp.ci/https://raw.githubusercontent.com/qichiyuhub/sbshell/refs/heads/master/debian/menu.sh"
+MAIN_SCRIPT_URL="https://raw.githubusercontent.com/qichiyuhub/sbshell/refs/heads/master/debian/menu.sh"
 
 # 脚本下载目录
 SCRIPT_DIR="/etc/sing-box/scripts"
